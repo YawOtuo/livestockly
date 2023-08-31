86ed098fad31fff86bb6e1429eb56926585af7da
YawOtuo <yotuo2003@gmail.com>
2023-08-31 16:41:52 +0000

    authentication and api reqs

--- src/weburl.js
+++ src/weburl.js
@@ -1,3 +1,3 @@
-// export const url = "https://boatey-farms-backend.herokuapp.com/"
+export const url = "https://boatey-farms-backend.herokuapp.com/"
 
-export const url = "http://localhost:8003/"
+// export const url = "http://localhost:8000/"
