1aafc4ffc3a2409cb3fc5ce8802556415d89362d
YawOtuo <yotuo2003@gmail.com>
2024-01-29 21:55:55 +0000

    padding

--- src/components/NotAcceptedIntoFarm.tsx
+++ src/components/NotAcceptedIntoFarm.tsx
@@ -2,7 +2,7 @@ import { Button } from "@mui/material";
 
 function NotAcceptedIntoFarm() {
   return (
-    <div className="flex flex-col gap-5 min-h-[70vh] items-center justify-center border-2 bg-green2">
+    <div className="flex flex-col gap-5 min-h-[70vh] items-center justify-center border-2 bg-green2 px-5">
       <p className="text-2xl font-semibold text-green1">
         Oops!!! It seems you have not yet been accepted into this farm
       </p>
