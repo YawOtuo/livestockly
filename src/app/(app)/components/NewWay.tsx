1bb3b83879b0d0325477e350583c38b0224c7ffd
YawOtuo <yotuo2003@gmail.com>
2024-09-26 23:20:43 +0000

    "Updated service worker file and added a line break in NewWay.tsx component"

--- src/app/(app)/components/NewWay.tsx
+++ src/app/(app)/components/NewWay.tsx
@@ -2,7 +2,7 @@ function NewWay() {
   return (
     <div className="py-20 my-20 flex items-center">
       <div className="text-3xl lg:text-5xl ">
-        A <span className="text-primary font-semibold">new</span> way to keep your records.{" "}
+        A <span className="text-primary font-semibold">new</span> way to keep your records.{" "} <br />
         <span className="text-primary font-semibold">Here is how....</span>
       </div>
     </div>
